cb8765a6138fc4dab8ea800d652874942180b1c9
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-07-10 17:17:23 -0500

    removed instance of BookingPage component from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <BookingPage />
 
 
         <a
