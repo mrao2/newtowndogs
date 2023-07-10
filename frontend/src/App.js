aa35d63db65d0daedc833d5803b986e53db2cc75
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-07-10 17:44:53 -0500

    updated styles for BookingPage; fixed Bootstrap import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import BookingPage from './BookingPage/BookingPage';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
 
+      <BookingPage />
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    </div >
   );
 }
 
--- frontend/src/BookingPage/BookingPage.js
+++ frontend/src/BookingPage/BookingPage.js
@@ -3,9 +3,7 @@ import React from 'react';
 import DatePicker from "react-datepicker";
 import './BookingPage.css';
 import Form from 'react-bootstrap/Form';
-
-
-
+import Button from 'react-bootstrap/Button';
 
 const BookingPage = () => {
 
@@ -13,18 +11,25 @@ const BookingPage = () => {
     return (
         <>
             <h1>Booking Page</h1>
+
+            <p>Please select the date you need a dog sitter.</p>
             <Form>
-                <div>
-                    <DatePicker selected={date} onChange={(date) => setDate(date)} />
+                <div id="appDatePicker">
+                    <DatePicker className="appDatePicker" selected={date} onChange={(date) => setDate(date)} />
                 </div>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Example textarea</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
-                </Form.Group>
+                <div className="clientEmail">
+                    <Form.Group className="clientEmail" controlId="clientEmail">
+                        <Form.Label>Email address: </Form.Label>
+                        <Form.Control type="email" placeholder="name@example.com" />
+                    </Form.Group>
+                </div>
+                <div className="appSpecialRequirements">
+                    <Form.Group className="appSpecialRequirements" controlId="appSpecialRequirements">
+                        <Form.Label>Special Requirements: </Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+                </div>
+                <Button variant="outline-primary">Submit Appointment Request</Button>
 
             </Form>
         </>
