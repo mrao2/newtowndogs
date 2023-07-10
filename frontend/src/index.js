db969b19c3e8a4e505805c4dac3f578ba5a8b1c9
emjayholla <maggie.hollander@gmail.com>
2023-07-09 21:35:31 -0500

    started profile page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Profile } from './profile/Profile'
 import reportWebVitals from './reportWebVitals';
+
+const favoriteNumber = 2
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Profile favoriteNumber={favoriteNumber} />
   </React.StrictMode>
 );
 
