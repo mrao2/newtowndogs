1322321f7c0b49fa22637f44d6835cfd120acb87
paul <paulentz@gmail.com>
2023-07-10 15:31:54 -0500

    got a barbones landing page and navbar up

--- frontend/src/Components/pages/Homepage.js
+++ frontend/src/Components/pages/Homepage.js
@@ -1,29 +1,39 @@
-let firstName = 'somethingverylong';
-let wordToReverse = 'ooooooooooooooooooocooooooooooooooo';
+import { useState } from "react";
 
+const project = {
+  title: "Welcome to Newtown Dogs",
+};
 
-function ifName (name) {
-    if (name.length > 10) {
-        return "Name is too long"
-    } else {
-        return name
-    }
+const breeds = [
+  { title: "Newfie", id: 1 },
+  { title: "Golden", id: 2 },
+  { title: "Lab", id: 3 },
+  { title: "Mix", id: 4 },
+  { title: "Husky", id: 5 },
+];
+
+const listItems = breeds.map((breed) => <li key={breed.id}>{breed.title}</li>);
+
+function Homepage() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <>
+      <h1>{project.title}!</h1>
+      <p>Our most common breeds</p>
+      <ul> {listItems}</ul>
+
+      <MyButton count={count} onClick={handleClick} />
+    </>
+  );
 }
 
-function FirstReverse(str) {
-
-    console.log(str);
-    arr = str.split('');
-    console.log(arr)
-    let revArr = arr.reverse();
-    str = revArr.join('');
-
-    return str;
-
+function MyButton({ count, onClick }) {
+  return <button onClick={onClick}>Clicked {count} times</button>;
 }
 
-
-
-console.log(FirstReverse(wordToReverse));
-
-console.log(ifName(firstName));
+export default Homepage;
