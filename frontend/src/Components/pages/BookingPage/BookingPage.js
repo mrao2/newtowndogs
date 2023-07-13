bb9345edfc9120f1237a269b915a8b8fc836a9d1
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-07-13 15:29:16 -0500

    continuing to experiment with time/date pickers

--- frontend/src/Components/pages/BookingPage/BookingPage.js
+++ frontend/src/Components/pages/BookingPage/BookingPage.js
@@ -5,7 +5,7 @@ import 'react-datepicker/dist/react-datepicker.css';
 import './BookingPage.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import TimePicker from './TimePicker';
+
 
 
 const BookingPage = () => {
@@ -14,20 +14,21 @@ const BookingPage = () => {
     const [startDate, setStartDate] = useState();
     const [endDate, setEndDate] = useState();
 
+
     const handleChange = (range) => {
         const [startDate, endDate] = range;
         setStartDate(startDate);
         setEndDate(endDate);
-    }
+    };
+
 
     return (
         <>
             <h1>Booking Page</h1>
 
-
             <Form>
                 <div className="clientEmail">
-                    <p>Please enter your contact email.</p>
+
                     <Form.Group className="clientEmail" controlId="clientEmail">
                         <Form.Label>Email address: </Form.Label>
                         <Form.Control type="email" placeholder="name@example.com" />
@@ -36,7 +37,6 @@ const BookingPage = () => {
                 <p>Please select the date(s) you need a dog sitter.</p>
                 <div id="appDatePicker">
                     <DatePicker className="appDatePicker" selected={startDate} onChange={handleChange} startDate={startDate} endDate={endDate} selectsRange />
-                    <TimePicker />
                 </div>
 
                 <div className="appSpecialRequirements">
--- frontend/src/Components/pages/BookingPage/TimePicker.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Datetime from 'react-datetime';
-import "react-datetime/css/react-datetime.css";
-
-export default function TimePicker() {
-    return <Datetime />;
-}
