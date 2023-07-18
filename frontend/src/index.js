0d550525ee9a8c7594aef36a071645b326e9ee5f
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-07-18 12:06:51 -0500

    'trying to fix errors'

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 // @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById('root'));
