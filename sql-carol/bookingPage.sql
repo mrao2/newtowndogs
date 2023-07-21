3fce7129168de8b34059be3042bce27fc0a0e004
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-07-20 20:06:10 -0500

    updated bookingPage.sql

--- sql-carol/bookingPage.sql
+++ sql-carol/bookingPage.sql
@@ -1,6 +1,6 @@
 CREATE TABLE appointments (
 	 appointment_id INT PRIMARY KEY AUTO_INCREMENT, 
-	 user_id INT 
+	 FOREIGN KEY (id) REFERENCES login_app(id), 
      start_time VARCHAR(255),
 	 end_time VARCHAR(255),
 	 start_date VARCHAR(255),
