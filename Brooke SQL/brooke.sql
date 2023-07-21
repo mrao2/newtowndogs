dc0ad6ffbaa27ac970cd814b77d1536cc421938c
brookebrown617 <brownbrooke617@gmail.com>
2023-07-20 20:09:54 -0500

    whoops

---
+++ Brooke SQL/brooke.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `login_app` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `hashed_password` varchar(128) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `firstname` varchar(45) NOT NULL,
+  `lastname` varchar(45) NOT NULL,
+  `phone` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `phone_UNIQUE` (`phone`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
