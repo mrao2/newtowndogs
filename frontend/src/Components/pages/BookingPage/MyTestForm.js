a35a67d9c3c95402d8c3ef57451d5f41c11b8ee7
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-07-26 19:25:06 -0500

    continuing to mess around with forms

--- frontend/src/Components/pages/BookingPage/MyTestForm.js
+++ frontend/src/Components/pages/BookingPage/MyTestForm.js
@@ -1,45 +1,11 @@
 import React, { useState } from 'react';
-import { Formik, FormikField } from "formik";
 
 
 const MyTestForm = () => {
-    const [form, setForm] = useState(new Formik({
-        initialValues: {
-            name: "",
-            email: "",
-            password: "",
-        },
-        onSubmit: (values) => {
-            // do something with the values
-        },
-    }));
+
 
     return (
-        <Formik form={form}>
-            {({ errors, handleChange, handleSubmit }) => (
-                <form onSubmit={handleSubmit}>
-                    <input
-                        name="name"
-                        type="text"
-                        // value={form.values.name}
-                        onChange={handleChange}
-                    />
-                    <input
-                        name="email"
-                        type="email"
-                        // value={form.values.email}
-                        onChange={handleChange}
-                    />
-                    <input
-                        name="password"
-                        type="password"
-                        // value={form.values.password}
-                        onChange={handleChange}
-                    />
-                    <button type="submit">Submit</button>
-                </form>
-            )}
-        </Formik>
+        <form></form>
     );
 
 }
