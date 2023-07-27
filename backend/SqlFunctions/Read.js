0e1031ec7fcf54afe6cc3122265d7554015bfa09
Dalton-Kujawa <daltonkujawa@yahoo.com>
2023-07-26 19:17:43 -0500

    cleaned out some commented code

--- backend/SqlFunctions/Read.js
+++ backend/SqlFunctions/Read.js
@@ -17,41 +17,4 @@ const Read = async (req, res, query) => {
           })
 };
 
-//   if (BlogId === "") {
-//     return new Promise((resolve, reject) => {
-//       const selectQuery = "SELECT * FROM blogs";
-//       connection.query(selectQuery, (err, results) => {
-//         if (err) {
-//           console.error("Error reading data: " + err.stack);
-//           reject(err);
-//           return;
-//         }
-
-//         console.log("Retrieved data:");
-//         resolve(results);
-//       });
-//     });
-//   } else {
-//     return new Promise((resolve, reject) => {
-//       const selectQuery = "SELECT * FROM blogs WHERE BlogId = ?";
-//       connection.query(selectQuery, [BlogId], (err, results) => {
-//         if (err) {
-//           console.error("Error fetching data:", err.stack);
-//           reject(err);
-//           return;
-//         } else {
-//           if (results.length === 0) {
-//             console.error("Blog Not Found");
-//             reject("Blog Not Found");
-//             return;
-//           } else {
-//             console.log("Retrieved data:");
-//             resolve(results);
-//           }
-//         }
-//       });
-//     });
-//   }
-// };
-
 module.exports = Read;
--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,6 @@ const express = require("express");
 const path = require("path");
 require("dotenv").config();
 
-const { writeFileSync } = require("fs");
-const crypto = require("crypto");
-const { stringify } = require("querystring");
-
 var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: process.env.Host,
@@ -32,15 +28,6 @@ app.get("/api/data", async (req, res) => {
         message: "User lists retrieved successfully"
       })
     })
-//   try {
-//     const sq = "SELECT * FROM blogs";
-//     const allBlogs = await read(req, res, sq);
-//     res.send(allBlogs);
-//   } catch (err) {
-//     // Handle errors here, if necessary
-//     console.error("Error retrieving data:", err);
-//     res.status(500).send("Error retrieving data");
-//   }
 });
 
 // API endpoint to fetch a specific blog by BlogId
@@ -60,12 +47,6 @@ app.get("/api/data/:BlogId", async (req, res) => {
 
 });
 
-// app.post('/api/data',(req, res) =>{
-//     req.body.id = crypto.randomUUID();
-//     jsonData.blogs.push(req.body);
-//     writeFileSync('./data/db.json', JSON.stringify(jsonData, undefined, 2));
-//     res.send(jsonData);
-// })
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
