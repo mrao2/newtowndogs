06281e840224698e5244ca8a6fb8d91417798e1b
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-08-01 11:48:07 -0500

    ]

--- frontend/src/Components/pages/BookingPage/AppSubmitted.js
+++ frontend/src/Components/pages/BookingPage/AppSubmitted.js
@@ -3,7 +3,7 @@ import React from "react";
 const AppSubmitted = () => {
     return (
         <>
-            <h1>Form Submitted!</h1>
+            <h1>Appointment Request Submitted!</h1>
         </>
     )
 }
