5bcbc29bec8aa566799b3bee4c40744d4fc81dee
brookebrown617 <brownbrooke617@gmail.com>
2023-08-02 06:56:06 -0500

    changed sql

--- Brooke SQL/brooke.sql
+++ Brooke SQL/brooke.sql
@@ -1,12 +1,11 @@
 CREATE TABLE `login_app` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY,
   `username` varchar(45) NOT NULL,
   `hashed_password` varchar(128) NOT NULL,
   `email` varchar(100) DEFAULT NULL,
   `first_name` varchar(45) NOT NULL,
   `last_name` varchar(45) NOT NULL,
   `phone` int NOT NULL,
-  PRIMARY KEY (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`),
   UNIQUE KEY `phone_UNIQUE` (`phone`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
