c348e8d89542da19887f86ce84d6d26054c395cb
paul <paulentz@gmail.com>
2023-08-06 16:22:44 -0500

    updated about us a bit more

--- frontend/src/Components/pages/AboutUs/AboutUs.js
+++ frontend/src/Components/pages/AboutUs/AboutUs.js
@@ -3,17 +3,30 @@ import "./AboutUs.css";
 const AboutUs = () => {
   return (
     <div className="aboutus">
-      <h1>We suck!</h1>
-      <p>(at being bad)</p>
+      <h1>About us</h1>
+      <p>
+        We suck! <br />
+        (at being bad)
+      </p>
       <img
+        className="husky_stare"
         src="https://cdn.stocksnap.io/img-thumbs/960w/husky-animal_J5DBCKIQNZ.jpg"
         alt="husky stare"
       />
-
+      <p>
+        Founded in 2023 Newtown dogs is the premier full service dog grooming
+        destination for the whole St Louis and St Charles metro region
+      </p>
       <img
+        className="golden_doodle"
         src="https://media.istockphoto.com/id/1306641965/photo/cobberdog-pup-on-white-background.jpg?s=612x612&w=0&k=20&c=yERA7k-SKw8A5wXq08aZSSde0Fng1gnHUVjF1tYEBQA="
-        alt=""
+        alt="golden_doodle"
       />
+      <p>
+        Our passionate team and years of professionalism will ensure an amazing
+        experience for you and your pet from start to finish
+      </p>
+      <p>We strive to continue providing excellent services and </p>
     </div>
   );
 };
