48f625fcbbca2f004a8c4458333bca9cba5f531c
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-08-08 18:44:32 -0500

    working through fetching data from database

--- frontend/src/Components/pages/AppointmentDislay/AppointmentDisplay.js
+++ frontend/src/Components/pages/AppointmentDislay/AppointmentDisplay.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import dog1 from './images/appDisplayDog.jpg';
 // import dog2 from './images/appDisplayDog2.jpg';
 import '../BookingPage/BookingDoggy.css'
-// import useFetch from "../../useFetch";
+import useFetch from "../../useFetch";
 import './AppointmentDisplay.css';
 
 const AppointmentDisplay = () => {
+
+    const { data: appointments, isPending, error } = useFetch('/api/appointments');
 
     return (
         <>
@@ -13,29 +15,59 @@ const AppointmentDisplay = () => {
             <br />
             <br />
             <h1 className="appHeader text-center">Appointment Requests</h1>
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading</div>}
+            {appointments}
             <div className="container">
                 <div class="row">
-                    <div className="col-sm-12 col-md-6 mt-3">
+                    <div className="col-sm-12 col-md-6 mt-3 border">
                         <h2 className="appContent border rounded text-center" >Appointment</h2>
                         <div className="appointmentParagraphs text-center">
-                            <p>Pet Parent Name: Davey Crockett</p>
-                            <p>Pet Name: Lassie Crockett</p>
-                            <p>Appointment Start Time: 8am</p>
-                            <p>Appointment End Time: 10am</p>
-                            <p>Appointment Start Date: 8/29/23</p>
-                            <p>Appointment End Date: 8/30/23</p>
+                            <p className="petParentName">Pet Parent Name: Davey Crockett</p>
+                            <p className="petName">Pet Name: Lassie Crockett</p>
+                            <p className="apptStartTime">Appointment Start Time: 8am</p>
+                            <p className="apptEndTime">Appointment End Time: 10am</p>
+                            <p className="apptStartDate">Appointment Start Date: 8/29/23</p>
+                            <p className="apptEndDate">Appointment End Date: 8/30/23</p>
                         </div>
                     </div>
-                    <div className="col-sm-12 col-md-6 appContent mt-3">
+                    <div className="col-sm-12 col-md-6 mt-3 border">
                         <h2 className="appContent border border-info rounded text-center">Appointment</h2>
 
                         <div className="appointmentParagraphs text-center">
-                            <p>Pet Parent Name: Davey Crockett</p>
-                            <p>Pet Name: Lassie Crockett</p>
-                            <p>Appointment Start Time: 8am</p>
-                            <p>Appointment End Time: 10am</p>
-                            <p>Appointment Start Date: 8/29/23</p>
-                            <p>Appointment End Date: 8/30/23</p>
+                            <p className="petParentName">Pet Parent Name: Davey Crockett</p>
+                            <p className="petName">Pet Name: Lassie Crockett</p>
+                            <p className="apptStartTime">Appointment Start Time: 8am</p>
+                            <p className="apptEndTime">Appointment End Time: 10am</p>
+                            <p className="apptStartDate">Appointment Start Date: 8/29/23</p>
+                            <p className="apptEndDate">Appointment End Date: 8/30/23</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div className="col-sm-12 col-md-6 mt-3 border">
+                        <h2 className="appContent border rounded text-center" >Appointment</h2>
+                        <div className="appointmentParagraphs text-center">
+                            <p className="petParentName">Pet Parent Name: Davey Crockett</p>
+                            <p className="petName">Pet Name: Lassie Crockett</p>
+                            <p className="apptStartTime">Appointment Start Time: 8am</p>
+                            <p className="apptEndTime">Appointment End Time: 10am</p>
+                            <p className="apptStartDate">Appointment Start Date: 8/29/23</p>
+                            <p className="apptEndDate">Appointment End Date: 8/30/23</p>
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md-6 mt-3 border">
+                        <h2 className="appContent border border-info rounded text-center">Appointment</h2>
+
+                        <div className="appointmentParagraphs text-center">
+                            <p className="petParentName">Pet Parent Name: Davey Crockett</p>
+                            <p className="petName">Pet Name: Lassie Crockett</p>
+                            <p className="apptStartTime">Appointment Start Time: 8am</p>
+                            <p className="apptEndTime">Appointment End Time: 10am</p>
+                            <p className="apptStartDate">Appointment Start Date: 8/29/23</p>
+                            <p className="apptEndDate">Appointment End Date: 8/30/23</p>
                         </div>
                     </div>
                 </div>
