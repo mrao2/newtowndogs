63eba23bac9bf0d503b4e9162ce12f9b4e242d0a
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-08-08 12:41:06 -0500

    updated appsubmit page and related css

--- frontend/src/Components/pages/BookingPage/AppSubmitted.js
+++ frontend/src/Components/pages/BookingPage/AppSubmitted.js
@@ -12,12 +12,11 @@ const AppSubmitted = () => {
             <br />
             <br />
             <br />
-            <br />
-            <br />
-            <br />
             <div>
                 <h1 className="appSubConf">Appointment Request Submitted!</h1>
                 <BookingDoggy />
+                <a class="bookingDoggySource" href="https://codepen.io/narendrashetty/pen/YwypNo" target="_blank" rel="noreferrer">Click here for Booking Doggy's original source code!</a>
+                <p class="bookingDoggySource">Booking Doggy was created by&nbsp;<a href="https://codepen.io/narendrashetty">Narendra N. Shetty</a>.</p>
             </div>
 
         </>
