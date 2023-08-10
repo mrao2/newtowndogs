3e7fd1bae3005b32fd2b2c4b9a9b6d13ab2e210f
Dalton-Kujawa <daltonkujawa@yahoo.com>
2023-08-10 11:30:25 -0500

    Fixed the create API endpoint

--- frontend/src/Components/pages/Blog/Create.js
+++ frontend/src/Components/pages/Blog/Create.js
@@ -15,7 +15,7 @@ const Create = () => {
 
     setIsPending(true);
 
-    fetch("/api/data", {
+    fetch("/blogs", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(blog),
