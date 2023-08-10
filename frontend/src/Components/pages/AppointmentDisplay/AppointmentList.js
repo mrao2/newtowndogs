017e9df666d0ca76b7fad452a44325116979a427
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-08-10 15:28:37 -0500

    fixed apt spacing & experimented w/ BD's css

--- frontend/src/Components/pages/AppointmentDisplay/AppointmentList.js
+++ frontend/src/Components/pages/AppointmentDisplay/AppointmentList.js
@@ -58,33 +58,33 @@ const AppointmentList = ({ appointments }) => {
 
 
     return (
-        <>
+        <>{appointments.data.map((appointment) => (
             <div class="appointment_list col-sm-12 col-md-6 mt-3">
-                {appointments.data.map((appointment) => (
-                    <div className="appointment-preview" key={appointment.appointment_id}>
+
+                <div className="appointment-preview" key={appointment.appointment_id}>
 
 
-                        <div className="border">
-                            <Link to={`/appointments/${appointment.appointment_id}`}>  <h2 className="appContent border rounded text-center">Appointment {appointment.appointment_id}</h2></Link>
-                            <div className="appointmentParagraphs  text-center">
-                                <p className="apptIsConsultation">Consultation Appointment? {appointment.is_consultation ? "Yes" : "No"}</p>
-                                <p className="petParentName">Pet Parent Name: {appointment.first_name} {appointment.last_name}</p>
-                                <p className="petName">Pet Name: ---</p>
-                                <p className="apptStartTime">Appointment Start Time: {appointment.start_time}</p>
-                                <p className="apptEndTime">Appointment End Time: {!appointment.end_date ? "---" : appointment.end_date.substring(0, 10)}</p>
-                                <p className="apptStartDate">Appointment Start Date: {appointment.start_date.substring(0, 10)}</p>
-                                <p className="apptEndDate">Appointment End Date: {!appointment.end_date ? "---" : appointment.end_date.substring(0, 10)}</p>
-                                <p className="apptDescription">Pet Parent Appointment Notes: {!appointment.description ? "---" : appointment.description}</p>
-                                <button className="confirmApptBtn btn"><a className="confirmRejectLink" href={`mailto:${appointment.email}?subject=NewTown Dogs - Confirming Your Appointment!`}>Confirm Appointment</a></button>
-                                <button className="rejectApptBtn btn"><a className="confirmRejectLink" href={`mailto:${appointment.email}?subject=NewTown Dogs - Appointment Cannot Be Confirmed Yet`}>Reject Appointment</a></button>
-                                <button value={appointment.appointment_id} onMouseEnter={() => (setAppointmentBeingDeleted(appointment.appointment_id), console.log(appointment.appointment_id))} onClick={(e) => handleTrashClick(e.target.value)}>
-                                    <FontAwesomeIcon className="trashCan" icon={faTrashCan} /></button>
-                            </div>
+                    <div className="border">
+                        <Link to={`/appointments/${appointment.appointment_id}`}>  <h2 className="appContent border rounded text-center">Appointment {appointment.appointment_id}</h2></Link>
+                        <div className="appointmentParagraphs  text-center">
+                            <p className="apptIsConsultation">Consultation Appointment? {appointment.is_consultation ? "Yes" : "No"}</p>
+                            <p className="petParentName">Pet Parent Name: {appointment.first_name} {appointment.last_name}</p>
+                            <p className="petName">Pet Name: ---</p>
+                            <p className="apptStartTime">Appointment Start Time: {appointment.start_time} </p>
+                            <p className="apptEndTime">Appointment End Time: {!appointment.end_date ? "---" : appointment.end_date.substring(0, 10)}</p>
+                            <p className="apptStartDate">Appointment Start Date: {appointment.start_date.substring(0, 10)}</p>
+                            <p className="apptEndDate">Appointment End Date: {!appointment.end_date ? "---" : appointment.end_date.substring(0, 10)}</p>
+                            <p className="apptDescription">Pet Parent Appointment Notes: {!appointment.description ? "---" : appointment.description}</p>
+                            <button className="confirmApptBtn btn"><a className="confirmRejectLink" href={`mailto:${appointment.email}?subject=NewTown Dogs - Confirming Your Appointment!`}>Confirm Appointment</a></button>
+                            <button className="rejectApptBtn btn"><a className="confirmRejectLink" href={`mailto:${appointment.email}?subject=NewTown Dogs - Appointment Cannot Be Confirmed Yet`}>Reject Appointment</a></button>
+                            <button value={appointment.appointment_id} onMouseEnter={() => (setAppointmentBeingDeleted(appointment.appointment_id), console.log(appointment.appointment_id))} onClick={(e) => handleTrashClick(e.target.value)}>
+                                <FontAwesomeIcon className="trashCan" icon={faTrashCan} /></button>
                         </div>
                     </div>
-                ))}
-            </div>
+                </div>
 
+            </div>
+        ))}
         </>
 
     )
