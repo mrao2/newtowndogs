5297abd14f7291410de2db19ec219fa97c25027d
paul <paulentz@gmail.com>
2023-08-11 11:59:05 -0500

    updated comment_body to 4000 comments.sql

--- SQL/6_comments.sql
+++ SQL/6_comments.sql
@@ -2,7 +2,7 @@ CREATE TABLE comments (
   CommentId INT AUTO_INCREMENT PRIMARY KEY,
   BlogId INT,
   Comment_Author VARCHAR(255),
-  Comment_Body VARCHAR(400),
+  Comment_Body VARCHAR(4000),
   username VARCHAR (45),
    INDEX (username),
    INDEX (BlogId),
