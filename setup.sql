72a1dfd6e27a81d5f3c0fe245d0bff72ae756141
paul <paulentz@gmail.com>
2023-08-24 00:36:59 -0500

    updated setup.sql

--- setup.sql
+++ setup.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `login_app` (
   email varchar(100) DEFAULT NULL,
   first_name varchar(45),
   last_name varchar(45),
-  phone int,
+  phone BIGINT,
   UNIQUE KEY username_UNIQUE (username),
   UNIQUE KEY phone_UNIQUE (phone),
   PRIMARY KEY (id),
@@ -17,38 +17,29 @@ CREATE TABLE IF NOT EXISTS `login_app` (
 );
 
 CREATE TABLE IF NOT EXISTS `user_profile` (
-    id INTEGER,
-    first_name varchar(45),
-    last_name varchar(45),
-    email VARCHAR (100),
-    username varchar(45),
-    ownerAddress VARCHAR (100),
-    ownerCity VARCHAR (30),
-    ownerState VARCHAR (2),
-    ownerZip INTEGER,
-    ownerEmail VARCHAR (50),
-    dogName VARCHAR (25),
-    dogBreed VARCHAR (30),
-    dogGender VARCHAR (6),
-    dogColor VARCHAR (30),
-    dogBirthdate VARCHAR (5),
-    dogAllergies VARCHAR (200),
-    dogWeight INTEGER (3),
-    dogFriendly BOOLEAN,
-    amtWalks INTEGER (2),
-    amtMeals INTEGER (2),
-    dogPottyTrained BOOLEAN,
-    dogFixed BOOLEAN,
+    id int AUTO_INCREMENT,
+    login_app_id INTEGER,
+    owner_address VARCHAR (100),
+    owner_city VARCHAR (30),
+    owner_state VARCHAR (2),
+    owner_zip INTEGER,
+    dog_name VARCHAR (25),
+    dog_age INTEGER (2),
+    dog_breed VARCHAR (30),
+    dog_gender VARCHAR (6),
+    dog_color VARCHAR (30),
+    dog_birthdate VARCHAR (5),
+    dog_allergies VARCHAR (200),
+    dog_weight INTEGER (3),
+    dog_friendly BOOLEAN,
+    amt_walks INTEGER (2),
+    amt_meals INTEGER (2),
+    amt_per_meal INTEGER (2),
+    dog_potty_trained BOOLEAN,
+    dog_fixed BOOLEAN,
+    UNIQUE KEY login_id_UNIQUE (login_app_id),
     INDEX (id),
-    INDEX (first_name),
-    INDEX (last_name),
-    INDEX (email),
-    INDEX (username),
-    FOREIGN KEY (id) REFERENCES login_app(id),
-    FOREIGN KEY (first_name) REFERENCES login_app(first_name),
-    FOREIGN KEY (last_name) REFERENCES login_app(last_name),
-    FOREIGN KEY (email) REFERENCES login_app(email),
-    FOREIGN KEY (username) REFERENCES login_app(username)
+    FOREIGN KEY (login_app_id) REFERENCES login_app(id)
 );
 
 CREATE TABLE IF NOT EXISTS blogs (
@@ -92,25 +83,25 @@ CREATE TABLE IF NOT EXISTS appointments (
     FOREIGN KEY (username) REFERENCES login_app(username)
 );
 
-CREATE TABLE IF NOT EXISTS homepage (
-  breedid int AUTO_INCREMENT PRIMARY Key,
-  title VARCHAR(255) NOT NULL,
-  description VARCHAR(255) NOT NULL,
-  breed VARCHAR (255)
-); 
-
 CREATE TABLE IF NOT EXISTS comments (
   CommentId INT AUTO_INCREMENT PRIMARY KEY,
   BlogId INT,
   Comment_Author VARCHAR(255),
   Comment_Body VARCHAR(4000),
   username VARCHAR (45),
+  Comment_Date DATETIME,
    INDEX (username),
    INDEX (BlogId),
   FOREIGN KEY (username) REFERENCES login_app(username),
   FOREIGN KEY (BlogId) REFERENCES blogs(BlogId) ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS homepage (
+  breedid int AUTO_INCREMENT PRIMARY Key,
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  breed VARCHAR (255)
+); 
 
 CREATE TABLE IF NOT EXISTS images (
   ImageId INT AUTO_INCREMENT PRIMARY KEY,
